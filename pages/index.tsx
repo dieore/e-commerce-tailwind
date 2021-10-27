eb6abd74e35957e5b6db696b6260b651ad9c5624
dieore <87033669+dieore@users.noreply.github.com>
2021-10-27 16:46:39 -0300

    Logic added to look for errors in API.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,41 @@
 import type { NextPage } from 'next'
 
-const Home: NextPage = () => {
+interface Products {
+    nombre: string;
+    descripcion: string;
+    stock: number;
+    precio: number;
+    imagen: string;
+    promo: boolean;
+    categoriaId: string;
+    id: string;
+}
+
+type Prop = {
+  data: Products[]
+}
+
+const Home: NextPage<Prop> = ({ data }) => {
   return (
     <div className="h-72">
-      Soy la home
+      {
+        data.map(d => (
+          <li>
+            {d.nombre}
+          </li>
+        ))
+      }
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api/hello")
+  const data = await res.json();
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  }
 }
 
 export default Home;
