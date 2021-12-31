af4e4539f388363d89e599c44c54833f0247a4e7
dieore <orefici.diego@gmail.com>
2021-12-31 14:50:22 -0300

    Context updated for currentUser. Account page added. Just starting.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,36 @@
-import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
-import Nav from '../components/Nav'
-import Footer from '../components/Footer'
-import AppContext from '../AppContext'
-import { useState } from 'react'
+import 'tailwindcss/tailwind.css';
+import type { AppProps } from 'next/app';
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+import AppContext from '../AppContext';
+import { useState } from 'react';
+import { User as FirebaseUser } from "firebase/auth";
+import { auth } from "../lib/firebase.config";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [openMenu, setOpenMenu] = useState<boolean>(false);
+  const [currentUser, setCurrentUser] = useState<FirebaseUser | null>(null);
+
+  auth.onAuthStateChanged((user) => {
+    console.log(user, "el user")
+    if (user) {
+      setCurrentUser(user);
+    } else {
+      setCurrentUser(null);
+    }
+  })
 
   return (
     <>
       <AppContext.Provider value={{
         openMenu,
-        setOpenMenu
+        setOpenMenu,
+        currentUser
       }}
       >
         <Nav />
-        <Component {...pageProps}  />
+        <Component {...pageProps} />
         <Footer />
       </AppContext.Provider>
     </>
---
+++ pages/account.tsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import AppContext from "../AppContext";
+
+const Account = () => {
+    const { currentUser } = useContext<any>(AppContext);
+
+    return (
+        <div>
+            Aca tendria que hacer una big card {currentUser.uid}
+        </div>
+    )
+};
+
+export default Account;
